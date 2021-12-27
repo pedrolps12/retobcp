eda28481a7dcc98d84e19ecc5092218196a845c1
PEDRO <pedrolps12@gmail.com>
2021-12-27 11:46:08 -0500

    Add files with data to h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO exchange_currency(code, description,  amount) VALUES ( 'PEN', 'SOLES', 0.25);
+INSERT INTO exchange_currency(code, description,  amount) VALUES ( 'USD', 'DOLARES', 1.00);
+INSERT INTO exchange_currency(code, description,  amount) VALUES ( 'CLP', 'PESOS CHILENOS', 0.0011);
+
+INSERT INTO users(username, password, rol) VALUES ( 'admin', '$2a$12$K.IBzmo1J1f5JImVQjhcSesYBnBFFjxWDBnOwQK5Q2LBME2xhY94.', 'ADMIN');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS exchange_currency;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE exchange_currency (
+  id uuid DEFAULT random_uuid() PRIMARY KEY,
+  code VARCHAR(10) NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  amount NUMERIC(10,4) NOT NULL
+);
+
+CREATE TABLE users (
+  id uuid DEFAULT random_uuid() PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(250) NOT NULL,
+  rol  VARCHAR(200) not null
+);
